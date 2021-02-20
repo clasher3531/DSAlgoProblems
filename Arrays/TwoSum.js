4571f07df9f297dbcb0415d5c7e26ee031613077
C_L_A_S_H_E_R <nikhil.a.joshi@capgemini.com>
2021-02-20 15:40:29 +0530

    Two Sum Problem

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -1,6 +1,6 @@
 // Given an array of integers nums and an integer target, 
 // return indices of the two numbers such that they add up to target.
-// Problem Link : https://leetcode.com/problems/two-sum/solution/
+// Problem Link : https://leetcode.com/problems/two-sum/
 
 var twoSum = function(nums, target) {
 
