ef193ae88706c297d7d8f4c2d299f26fd205d2c8
Nikhil Joshi <nikhil.a.joshi@capgemini.com>
2021-03-18 15:17:16 +0530

    Adding Linked List Cycle Problem

---
+++ LinkedList/LinkedListCycle.js
@@ -0,0 +1,26 @@
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+// There is a cycle in a linked list if there is some node in the list that can be reached 
+// again by continuously following the next pointer. Internally, pos is used to denote 
+// the index of the node that tail's next pointer is connected to. 
+// Note that pos is not passed as a parameter.
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+// Problem Link : https://leetcode.com/problems/linked-list-cycle/
+
+// By using two pointers slow and fast
+
+const hasCycle = head => {
+    let p1 = head
+    let p2 = head
+    
+    while (p2 && p2.next && p2.next.next) {
+        p1 = p1.next
+        p2 = p2.next.next
+        
+        if (p1 === p2) {
+            return true
+        }
+    }
+    
+    return false
+}
